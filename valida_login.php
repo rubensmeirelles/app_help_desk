1c9a60f08f268c160a7951e5e3147f4927c425bf
rubens.meirelles <rrubens.meirelles@gmail.com>
2024-04-23 21:38:23 -0300

    Validação de login

--- valida_login.php
+++ valida_login.php
@@ -1,3 +1,24 @@
 <?php
-    
+    //VARIÁVEL PARA VERIFICAR SE A AUTENTICAÇÃO FOI REALIZADA
+    $usuario_autenticado = false;
+
+    //BANCO DE DADOS MOCK
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    //PERCORRER O ARRAY
+    foreach($usuarios_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        };
+    }
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
 ?>
